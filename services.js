896b9610aa4a21533ade138806b3b6b95f016618
Davide D'Antonio <davide.dantonio@webeetle.com>
2019-11-26 14:54:32 +0100

    enh

--- services.js
+++
@@ -1,8 +0,0 @@
-import { plugin } from './src/index'
-
-export default (instance, name) => {
-  instance.decorate(name, {
-    getName: async _ => instance.customerService2.getName(),
-    getSurname: _ => instance.log.info('My Surname is DAntonio')
-  })
-}
--- test/sdk.test.js
+++ test/sdk.test.js
@@ -3,12 +3,5 @@ import service from '../services'
 import service2 from '../services2'
 
 it('We can check if the consumer called the class constructor', () => {
-  const mySdk = new SDK({
-    name: 'KamAppSDK',
-    logLevel: 'info',
-  });
 
-  mySdk.addService('customerService', service)
-  mySdk.addService('customerService2', service2)
-  console.log(expect(mySdk.customerService.getName()))
 })
