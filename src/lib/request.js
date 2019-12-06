abab9be68d81c3f81ceeaa411bf992d2f64483ab
Davide D'Antonio <davide.dantonio@webeetle.com>
2019-12-06 10:09:34 +0100

    fix

--- src/lib/request.js
+++ src/lib/request.js
@@ -3,15 +3,7 @@
 const axios = require('axios')
 
 function buildRequest(instance) {
-  instance.decorate('request', axios.request)
-  instance.decorate('post', axios.post)
-  instance.decorate('get', axios.get)
-  instance.decorate('delete', axios.delete)
-  instance.decorate('head', axios.head)
-  instance.decorate('options', axios.options)
-  instance.decorate('put', axios.put)
-  instance.decorate('patch', axios.patch)
-  instance.decorate('getUri', axios.getUri)
+  instance.decorate('req', axios)
 }
 
 function init (instance) {
